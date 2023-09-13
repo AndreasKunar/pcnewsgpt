4cd3299c81435cdf4d83942ce40aba1f0fc8b402
Andreas Kunar <andreask@msn.com>
2023-09-13 14:53:24 +0200

    Umstellung PDF-Importer

--- import.py
+++ import.py
@@ -1,5 +1,6 @@
 """
 *** PCnewsGPT Wissensimporter - import.py ***
+    Änderung: V0.1.4.x - PyMuPDFLoader ersetzt pdfMiner.six
     Änderung: V0.1.3.x - append als funktion, besser lesbare, verständliche chunk-texte
 """
 
@@ -20,18 +21,18 @@ text_splitter_parameters = literal_eval(os_environ.get('TEXT_SPLITTER_PARAMETERS
 """
 Initial banner Message
 """
-print("\nPCnewsGPT Wissensimporter V0.1.3.1\n")
+print("\nPCnewsGPT Wissensimporter V0.1.4\n")
 
 """
 Map file extensions to document loaders and their arguments
 """
 from langchain.docstore.document import Document as langchain_Document
 from langchain.document_loaders import (
-    PDFMinerLoader,
+    PyMuPDFLoader,
     TextLoader
 )
 loader_mappings = {
-    ".pdf": (PDFMinerLoader, {}),
+    ".pdf": (PyMuPDFLoader, {}),
     ".txt": (TextLoader, {"encoding": "utf8"})
 }
 
@@ -101,94 +102,117 @@ def load_file(file_path: str) -> langchain_Document:
     loader_class, loader_args = loader_mappings[ext]
     loader = loader_class(file_path, **loader_args)
     lc_doc = loader.load()
-    assert len(lc_doc) == 1             # should only be one document per file up til now
 
-    # Tidy up PDFs - this might need optimization
+    # Tidy up PDFs - this might need optimization for some publisher-types
     if ext == ".pdf":
-        doc = lc_doc[0].page_content   #file_path still holds document source
-        # remove line-break hyphenations
-        doc = regex_sub(r'-\n+ *', '',doc)
-        # remove training spaces in lines
-        doc =doc.replace(' \n', '\n')
-        # remove excess spaces
-        doc = doc.replace('  ', ' ')
-        # substitute known ligatures & strange characters
-        doc = doc.replace('(cid:297)', 'fb')
-        doc = doc.replace('(cid:322)', 'fj')
-        doc = doc.replace('(cid:325)', 'fk')
-        doc = doc.replace('(cid:332)', 'ft')
-        doc = doc.replace('(cid:414)', 'tf')
-        doc = doc.replace('(cid:415)', 'ti')
-        doc = doc.replace('(cid:425)', 'tt')
-        doc = doc.replace('(cid:426)', 'ttf')
-        doc = doc.replace('(cid:427)', 'tti')
-        doc = doc.replace('\uf0b7', '*')
-        doc = doc.replace('•', '*')
-        doc = doc.replace('\uf031\uf02e', '1.')
-        doc = doc.replace('\uf032\uf02e', '2.')
-        doc = doc.replace('\uf033\uf02e', '3.')
-        doc = doc.replace('\uf034\uf02e', '4.')
-        doc = doc.replace('\uf035\uf02e', '5.')
-        doc = doc.replace('\uf036\uf02e', '6.')
-        doc = doc.replace('\uf037\uf02e', '7.')
-        doc = doc.replace('\uf038\uf02e', '8.')
-        doc = doc.replace('\uf039\uf02e', '9.')
-        doc = doc.replace('\uf0d8', '.nicht.')
-        doc = doc.replace('\uf0d9', '.und.')
-        doc = doc.replace('\uf0da', '.oder.')
-        doc = doc.replace('→', '.impliziert. (Mathematisch)')
-        doc = doc.replace('\uf0de', '.impliziert.')
-        doc = doc.replace('↔', '.äquivalent. (Mathematisch)')
-        doc = doc.replace('\uf0db', '.äquivalent.')
-        doc = doc.replace('≈','.annähernd.')
-        doc = doc.replace('\uf061', 'Alpha')
-        doc = doc.replace('β', 'Beta')
-        doc = doc.replace('\uf067', 'Gamma')
-        # substiture other strange characters
-        doc = doc.replace('€', 'Euro')
-        doc = doc.replace("„", '"')             # Anführungszeichen
-        doc = doc.replace("—", '"')             # m-dash
-        doc = doc.replace("'", '"')             # replace single with double quotes
-        doc = doc.replace("\t", " ")            # replace tabs with a space
-        doc = doc.replace("\r", "")             # delete carriage returns
-        doc = doc.replace("\v", "")             # delete vertical tabs
-        
-        # change single \n in content to " ", but not multiple \n
-        doc = regex_sub(r'(?<!\n)\n(?!\n)', ' ',doc)
-        # change multiple consecutive \n in content to just one \n
-        doc = regex_sub(r'\n{2,}', '\n',doc)
-        # remove strange single-characters with optional leading and trailing spaces in lines
-        doc = regex_sub(r'\n *(\w|\*) *\n', '\n',doc)
-        # remove strange single-characters with spaces inbetween texts
-        doc = regex_sub(r'((\w|/|:) +){3,}(\w|/|:)', '',doc)
-        # remove multiple blanks
-        doc = regex_sub(r'  +', ' ',doc)
-        
-        # split doc-content into pages and remove any trailing empty pages
-        pages =doc.split('\x0c')
-        while pages[-1] == '':
-            pages.pop()
-        # if there are no remaining pages, empty the text in lc_doc
-        if len(pages) == 0:
-            lc_doc[0].page_content = ""
-        # if its just one page, update lc_doc with processed content
-        elif len(pages) == 1:
-            lc_doc[0].page_content = pages[0]
-        # if there are muliple pages, create new lc_doc from non-empty pages
-        else:
-            lc_doc.pop()    # we need a new name + content as multiple pages
-            pg_num=1
-            for page in pages:
-                if page != '':    # only add non-empty pages, keep page numbering
-                    lc_doc.append( langchain_Document(
-                        page_content = page,
-                        metadata = {"source": f"{file_path} (page {pg_num})"}
-                    ))
-                pg_num += 1
+        for i in range(len(lc_doc)):
+            # Tidy up PDFs
+            doc = lc_doc[i].page_content
+            source = lc_doc[i].metadata['source']
+            page_num = lc_doc[i].metadata.get('page', None)
+            creationdate = lc_doc[i].metadata.get('creationdate', None)
 
+            # some Substutions are independent of PDF-Generators
+            # remove line-break hyphenations
+            doc = regex_sub(r'-\n+ *', '',doc)
+            # remove training spaces in lines
+            doc =doc.replace(' \n', '\n')
+            # substiture some strange characters
+            doc = doc.replace('€', 'Euro')
+            doc = doc.replace("„", '"')             # Anführungszeichen-Anfang
+            doc = doc.replace("—", '-')             # m-dash
+            doc = doc.replace("'", '"')             # replace single with double quotes
+            doc = doc.replace("\t", " ")            # replace tabs with a space
+            doc = doc.replace("\r", "")             # delete carriage returns
+            doc = doc.replace("\v", "")             # delete vertical tabs
+            # change single \n in content to " ", but not multiple \n
+            doc = regex_sub(r'(?<!\n)\n(?!\n)', ' ',doc)
+            # change multiple consecutive \n in content to just one \n
+            doc = regex_sub(r'\n{2,}', '\n',doc)
+            # remove strange single-characters with optional leading and trailing spaces in lines
+            doc = regex_sub(r'\n *(\w|\*) *\n', '\n',doc)
+            # remove strange single-characters with spaces inbetween texts
+            doc = regex_sub(r'((\w|/|:) +){3,}(\w|/|:)', '',doc)
+            # remove multiple blanks
+            doc = regex_sub(r'  +', ' ',doc)
+
+            # some Substutions are dependent on "publisher"
+            producer = lc_doc[i].metadata.get('producer', '*unknown*')
+            if producer.find('Publisher') >= 0:
+                # we got a Microsoft Publisher type file
+                # substitute known ligatures & strange characters
+                doc = doc.replace('(cid:297)', 'fb')
+                doc = doc.replace('(cid:322)', 'fj')
+                doc = doc.replace('(cid:325)', 'fk')
+                doc = doc.replace('(cid:332)', 'ft')
+                doc = doc.replace('(cid:414)', 'tf')
+                doc = doc.replace('(cid:415)', 'ti')
+                doc = doc.replace('(cid:425)', 'tt')
+                doc = doc.replace('(cid:426)', 'ttf')
+                doc = doc.replace('(cid:427)', 'tti')
+                doc = doc.replace('\uf0b7', '*')
+                doc = doc.replace('•', '*')
+                doc = doc.replace('\uf031\uf02e', '1.')
+                doc = doc.replace('\uf032\uf02e', '2.')
+                doc = doc.replace('\uf033\uf02e', '3.')
+                doc = doc.replace('\uf034\uf02e', '4.')
+                doc = doc.replace('\uf035\uf02e', '5.')
+                doc = doc.replace('\uf036\uf02e', '6.')
+                doc = doc.replace('\uf037\uf02e', '7.')
+                doc = doc.replace('\uf038\uf02e', '8.')
+                doc = doc.replace('\uf039\uf02e', '9.')
+                doc = doc.replace('\uf0d8', '.nicht.')
+                doc = doc.replace('\uf0d9', '.und.')
+                doc = doc.replace('\uf0da', '.oder.')
+                doc = doc.replace('→', '.impliziert. (Mathematisch)')
+                doc = doc.replace('\uf0de', '.impliziert.')
+                doc = doc.replace('↔', '.äquivalent. (Mathematisch)')
+                doc = doc.replace('\uf0db', '.äquivalent.')
+                doc = doc.replace('≈','.annähernd.')
+                doc = doc.replace('\uf061', 'Alpha')
+                doc = doc.replace('β', 'Beta')
+                doc = doc.replace('\uf067', 'Gamma')
+            elif producer.find('Print To PDF') >= 0:
+                doc = doc.replace('�', '_')
+
+            # split doc-content into pages and remove any trailing empty pages
+            pages =doc.split('\x0c')
+            while (len(pages) > 1) and (pages[-1] == ''):
+                pages.pop()
+            # if there are no remaining pages, empty the text in lc_doc
+            if len(pages) == 0:
+                lc_doc[i].page_content = ""
+            # if its just one page, update lc_doc with processed content
+            elif len(pages) == 1:
+                lc_doc[i].page_content = pages[0]
+                lc_doc[i].metadata = {"source": f"{source}"}
+                if page_num is not None:
+                    lc_doc[i].metadata.update({"page": f"{page_num+1}"})
+                if creationdate is not None:
+                    lc_doc[i].metadata.update({"creationdate": f"'{regex_sub(r'[^0-9]','',creationdate)[:8]}'" })
+            # if there are muliple pages, create new lc_doc from non-empty pages
+            else:
+                del lc_doc[i]    # we need a new name + content as multiple pages
+                pg_num=0
+                for page in pages:
+                    if page != '':    # only add non-empty pages, keep page numbering
+                        metadata = {"source": f"{source}", "page": f"{pg_num+1}"}
+                        if creationdate is not None:
+                            metadata.update({"creationdate": f"'{regex_sub(r'[^0-9]','',creationdate)[:8]}'" })
+                        lc_doc.insert(i, langchain_Document(
+                            page_content = page,
+                            metadata=metadata
+                        ))
+                        i+=1
+                    pg_num += 1
+
+    # remove empty documents
+    docs=[]
+    for i in range(len(lc_doc)):
+        if lc_doc[i].page_content != '':
+            docs.append(lc_doc[i])
     # return the loaded doc               
-    return lc_doc
-
+    return docs
 
 """
 Load + process all documents
@@ -227,15 +251,15 @@ for idx,file_path in enumerate(file_paths):
     total_chunks += len(chunks)
     # tidying-up chunk text and numbering of chunks per source
     n_chunk=1
-    chunk_name=chunks[0].metadata["source"]
+    chunk_metadata=chunks[0].metadata
     for i in range(len(chunks)):
-        # new chunk name if source is different
-        if chunk_name!=chunks[i].metadata["source"]:
+        # new chunk begins if metadata is different
+        if chunk_metadata!=chunks[i].metadata:
             n_chunk=1
-            chunk_name=chunks[i].metadata["source"]
+            chunk_metadata=chunks[i].metadata
         else:
             n_chunk += 1
-        chunks[i].metadata["source"]= f'{chunks[i].metadata["source"]} (chunk {n_chunk})'
+        chunks[i].metadata.update({"chunk": f"{n_chunk}"})
 
         # final tidying-up of chunk text - needed because of SpaCy weirdnesses
         txt = chunks[i].page_content
